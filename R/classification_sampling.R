9d9b94278d2c0bc5d859815f84739bcd05a084bb
Tim <tim.kreitner@googlemail.com>
2018-02-20 15:59:34 +0100

    Initial commit
    Starting the project

---
+++ R/classification_sampling.R
@@ -0,0 +1,18 @@
+# Necessary Sampling Techniques for classification
+
+# General Functions that will be reused
+find_next_max <- function(input) {
+
+}
+
+find_next_min <- function(input) {
+
+}
+
+
+
+# 2D Sampling: Rainflow analysis
+
+rainflow <- function(input) {
+    rnorm(input)
+}
