495e633c0a4ffc4141e91a7081e986a1eccc66b4
Tim <tim.kreitner@googlemail.com>
2018-02-22 12:13:10 +0100

    working extreme point reduction

--- R/classification_sampling.R
+++ R/classification_sampling.R
@@ -1,41 +1,38 @@
 # Necessary Sampling Techniques for classification
 
 # General Functions that will be reused
-identify_extrema <- function(time_series) {
-    time_series <- as.zoo(time_series)
+identify_extrema <- function(time_series_local) {
+    time_series_local <- as.zoo(time_series_local)
 
     # minima
-    mins <- rollapply(time_series[,2], 3, function(x) which.min(x)==2)
+    mins <- rollapply(time_series_local[,2], 3, function(x) which.min(x)==2)
     # maxima
-    maxs <- rollapply(time_series[,2], 3, function(x) which.max(x)==2)
+    maxs <- rollapply(time_series_local[,2], 3, function(x) which.max(x)==2)
 
     extrema <- mins | maxs
+
+    extrema <- c(TRUE, extrema, TRUE)
     #return DF with time and return points
-    time_series_reduced <- as.data.frame(time_series[extrema])
-}
-
-classify_points <- function(input_points, n = 16) {
-    #if amount of classes not specified, display warning that default has been used
-    max_val <- max(input_points)
-    min_val <- min(input_points)
-    step_width <- (max_val - min_val) / n
-
-    cat_bounds <- c(min_val)
-
-    for (i in i:n) {
-        cat_bounds[i] <- cat_bounds[i-1] + step_width
-    }
-
+    time_series_reduced <- as.data.frame(time_series_local[extrema])
 
 }
-
 
 # 2D Sampling: Rainflow analysis
 
 rainflow <- function(time_series, n = 16) {
-    #if amount of classes not specified, display warning that default has been used
+    # if amount of classes not specified, display warning that default has been used
 
     extremas <- identify_extrema(time_series)
     extremas[,3] <- classify(extremas[,2], "lin", breaks = n)$index
+
+    breaks <- classify(extremas[,2], "lin", breaks = n)$bb
+
+    # rainflow matrix from (coloumns) -> to (rows)
+    rainflow <- matrix(0, n, n)
+
+    # while loop for counting
+    while (T) {
+
+    }
 
 }
--- README.md
+++ README.md
@@ -5,14 +5,22 @@ A R package with useful calculations for endurance-strength of ferrous materials
 ## Calculations
 
 ### Rainflow Matrix
+*Inputs:*
+- stress values: 2 coloumn data-frame with order-id / time-stamp and stress values
+- n - Amount of categories to be used
+*Returns:*
+- n x n Matrix 
 
-- rainflow counting
-- rainflow matrix
+**Rainflow Counting:** Use the 4-point algorithm to determine the individual stress cycles. 
+
+**Rainflow Matrix:** Denotes the stress cycles with the from and to categories. Counting each cycle with the [i,j] position.
 
 ## Plots
 
 ### Rainflow Plot (2D - Tile)
-A 2D tile plot, based on the rainflow matrix, showing the distribution of the external stress.
+
+A 2D tile plot, based on the *rainflow matrix*, showing the distribution of the external stress. 
 
 ### Woehler Curve
+
 Standard Woehler curve.
